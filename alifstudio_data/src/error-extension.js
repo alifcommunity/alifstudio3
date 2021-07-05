a5db2b2b5285236363941106bae6a1861fe52230
Mohammed Samir <ms.2052001@gmail.com>
2021-07-05 23:47:33 +0200

    إظهار الأخطاء  في المحرر،#3

---
+++ alifstudio_data/src/error-extension.js
@@ -0,0 +1,71 @@
+
+/**
+ * All the logic is that we need a StateField extension
+ * to be attached to the EditorState when creatting the
+ * EditorView in ./index.js. This StateField extension
+ * will handle some StateEffects, we can send these
+ * effect through the transaction.
+ */
+
+import { StateField, StateEffect } from "@codemirror/state";
+import { EditorView, Range, Decoration } from "@codemirror/view";
+
+// Effects can be attached to transactions to communicate with the extension
+export const addErrorEffect = StateEffect.define();
+export const filterErrorsEffect = StateEffect.define();
+
+// This value must be added to the set of extensions to enable this
+export const errorFieldExtension = StateField.define({
+  // Start with an empty set of decorations
+  create() {
+    return Decoration.none;
+  },
+
+  /**
+   * This is called whenever the editor updates—it computes the new set
+   * @param {RangeSet} value https://codemirror.net/6/docs/ref/#rangeset.RangeSet
+   * @param {Transaction} tr https://codemirror.net/6/docs/ref/#state.Transaction
+   */
+  update(value, tr) {
+    // Move the decorations to account for document changes
+    value = value.map(tr.changes);
+    // If this transaction adds or removes decorations, apply those changes
+    for (let effect of tr.effects) {
+      if (effect.is(addErrorEffect))
+        value = value.update({ add: effect.value, sort: true });
+      else if (effect.is(filterErrorsEffect))
+        value = value.update({ filter: effect.value });
+    }
+    return value;
+  },
+  // Indicate that this field provides a set of decorations
+  provide: (f) => EditorView.decorations.from(f),
+});
+
+/**
+ * Add errors to the ciew
+ * @param {Array<{ line: number, msg: string }>} errors
+ */
+export function addErrors(errors, view) {
+  const errorDocorations = errors.map((err) => {
+    const strikeMark = Decoration.mark({
+      // these values used in ./style.css
+      class: "alif-code-error",
+      attributes: { "data-error-message": err.msg },
+    });
+    const line = view.state.doc.lineAt(err.line);
+    return strikeMark.range(line.from, line.to);
+  });
+
+  view.dispatch({ effects: addErrorEffect.of(errorDocorations) });
+}
+
+/**
+ * clear all error set by `addErrors` function
+ * @param {EditorView} view the editor view to disptach the transaction
+ */
+export function clearErrors(view) {
+  view.dispatch({
+    effects: filterErrorsEffect.of(() => false),
+  });
+}
--- alifstudio_data/src/index.js
+++ alifstudio_data/src/index.js
@@ -6,6 +6,7 @@ import { lineNumbers } from "@codemirror/gutter";
 import { StreamLanguage } from "@codemirror/stream-parser";
 import { javascript } from "@codemirror/lang-javascript";
 import { simpleMode } from "@codemirror/legacy-modes/mode/simple-mode";
+import { addErrors, clearErrors, errorFieldExtension } from "./error-extension";
 import alifSimpleModeStates from "./alif-simple-mode-states";
 
 import "./styles.css";
@@ -18,31 +19,46 @@ function toArNum(num) {
 }
 
 class AlifCodemirrorEditor_V6 extends EditorView {
+  /**
+   * Construct a new alif editor, options interface is `{
+   *   parent: DOMElement;
+   *   value?: string;
+   *   onChange: (code) => void
+   * }`
+   * @param {object} options
+   */
   constructor(options) {
-    let { parent, value } = options;
+    let { parent, value, onChange } = options;
+
+    if (!parent) throw new Error("parent should be provided through `options`");
+
+    const extensions = [
+      basicSetup,
+      oneDark,
+      keymap.of([defaultTabBinding]),
+      lineNumbers({ formatNumber: (lineNo) => toArNum(lineNo) }),
+      StreamLanguage.define(simpleMode(alifSimpleModeStates)),
+      errorFieldExtension,
+    ];
+
+    // TODO: create a system to handle events, to add them and to turn them off
+    const onChangeExtension =
+      onChange &&
+      EditorView.updateListener.of(
+        (update) => update.docChanged && onChange(update)
+      );
+
     super({
       state: EditorState.create({
         doc: value,
-        extensions: [
-          basicSetup,
-          oneDark,
-          keymap.of([defaultTabBinding]),
-          lineNumbers({ formatNumber: (lineNo) => toArNum(lineNo) }),
-          StreamLanguage.define(simpleMode(alifSimpleModeStates)),
-          EditorView.updateListener.of((update) => {
-            if (update.docChanged) {
-              // KeyUp
-              if (typeof alif_code_OnChange === "function")
-                setTimeout(alif_code_OnChange, 0);
-            } else {
-              // Mouse Click
-              // ...
-            }
-          }),
-        ],
+        extensions: [...extensions, onChangeExtension],
       }),
       parent,
     });
+
+    this.options = options;
+    this.events = { onChange };
+    this.extensions = extensions;
   }
 
   setCode(code) {
@@ -62,30 +78,49 @@ class AlifCodemirrorEditor_V6 extends EditorView {
 }
 
 const parent = document.querySelector(".main-editor-container"); // get by ID is faster?
-const editor = new AlifCodemirrorEditor_V6({ parent });
+const editor = new AlifCodemirrorEditor_V6({
+  parent,
+  onChange: onAlifCodeChange,
+});
 
-export function أضف_كود_64(codeBase64) {
+function onAlifCodeChange(update) {
+  const code = update.state.doc.toString();
+  console.log("تم التغيير");
+  console.log(code);
+  // CodeMirror OnChange
+  webui_event && webui_event("تم التغيير");
+}
+
+export function اضف_كود_64(codeBase64) {
   // Base64 to UTF8
   let code = decodeURIComponent(escape(window.atob(codeBase64.trim())));
   code = code.replace(/\r/g, ""); // Remove all 0x0D [\r]
   editor.setCode(code.trim());
 }
 
-export function أضف_كود(code) {
+export function اضف_كود(code) {
   // UTF8
   code = code.replace(/\r/g, ""); // Remove all 0x0D [\r]
   editor.setCode(code.trim());
 }
 
-export function قرأة_كود() {
+export function قراءة_كود() {
   // Read
   let code = editor.getCode();
   code = code.replace(/\r/g, ""); // Remove all 0x0D [\r]
   return code;
 }
 
-export function خطأ(line_number, error_description) {
+export function خطأ(lineNumber, errorDescription) {
   // Show an error in Codemirror
   // Link: https://codemirror.net/6/docs/migration/
   // Section: Marked Text
+  addErrors([{ line: lineNumber, msg: errorDescription }], editor);
 }
+
+export function نظف_من_الأخطاء() {
+  // remove all error set by خطأ function
+  clearErrors(editor);
+}
+
+
